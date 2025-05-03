feae92080c68ce7ef77dbec7df8700b7bb54f678
Gabriel Angelo <gabrielangelolira@gmail.com>
2025-05-03 11:40:34 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo, Moon } from "phosphor-react"
+import { FacebookLogo, InstagramLogo, LinkedinLogo, Moon, Sun } from "phosphor-react"
 import Button from "./components/Button"
+import { useState } from "react";
 
 function App() {
+  function getInitialDarkMode() {
+    const darkModeLocalStorage = localStorage.getItem("darkMode");
+
+    return darkModeLocalStorage === "true" ? true : false;
+  }
+
+
+  const [darkMode, setDarkMode] = useState(getInitialDarkMode());
+
+  function redirectPage(url) {
+    window.open(url, "_blank");
+  }
+
+  function toggleDarkMode() {
+    setDarkMode(!darkMode);
+
+    // Set in Local Storage
+    localStorage.setItem("darkMode", !darkMode);
+  }
+
+  console.log(darkMode);
+
   return (
-    <div className="w-full h-screen bg-white">
-      <header className="bg-white flex justify-end p-4">
-        <button className="cursor-pointer">
-          <Moon size={16} />
-        </button>
+    <div className={`w-full h-screen ${darkMode ? "bg-gray-900" : "bg-white"}`}>
+      <header className={`flex justify-end p-4 ${darkMode ? "text-white" : "text-black"}`}>
+        <Button variant="secondary" onClick={toggleDarkMode}>
+          {darkMode ? <Moon size={16} /> : <Sun size={16} />}
+        </Button>
       </header>
       <main className="w-full h-full flex justify-center items-center">
-        <section className="border border-gray-400 bg-white p-6 rounded-lg w-fit flex flex-col justify-center items-center gap-4">
+      <section className={`border border-gray-400 ${darkMode ? "bg-gray-900 text-white" : "bg-white"} p-6 rounded-lg w-fit flex flex-col justify-center items-center gap-4`}>
+
           <img className="w-32 h-32 rounded-full border-4 border-gray-300" src="https://github.com/gabrielangelo0.png" alt="Foto de perfil do usuário" />
 
           <h1 className="font-bold text-2xl">Gabriel Angelo</h1>
@@ -20,17 +44,17 @@ function App() {
 
           {/* Seção de Links */}
           <div className="space-y-4">
-            <Button>
+            <Button onClick={() => redirectPage("https://www.instagram.com/gabriel.angelo__/")}>
               <InstagramLogo size={24} />
               <p>Instagram</p>
             </Button>
 
-            <Button>
+            <Button onClick={() => redirectPage("https://www.linkedin.com/in/gabriel-angelo0/")}>
               <LinkedinLogo size={24} />
               <p>Linkedin</p>
             </Button>
 
-            <Button>
+            <Button onClick={() => redirectPage("https://github.com/gabrielangelo0")}>
               <FacebookLogo size={24} />
               <p>Facebook</p>
             </Button>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,22 @@
-function Button({ children }) {
+function Button({ children, onClick, variant = "primary" }) {
+  // variant: primary, secondary
+
+  function getVariantClasses() {
+    switch (variant) {
+      case "primary":
+        return "flex gap-5 items-center justify-center border border-gray-300 cursor-pointer px-4 py-2 rounded-lg min-w-3xs"
+      case "secondary":
+        return "cursor-pointer hover:bg-gray-200 p-2 rounded-lg transition duration-300 ease-in-out"
+      default:
+        return "bg-yellow-300 text-white"
+    }
+  }
+  
   return (
-    <button className="flex gap-5 items-center justify-center border border-gray-300 cursor-pointer px-4 py-2 rounded-lg min-w-3xs">
+    <button
+      onClick={onClick}
+      className={getVariantClasses()}
+    >
       {children}
     </button>
   );
